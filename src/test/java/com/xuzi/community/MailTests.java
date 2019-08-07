81016185f3e52ffb7f6bd6ac132fbc2f16af595e
xuzixiang <1090765806@qq.com>
2019-08-07 13:57:30 +0800

    测试发送邮件功能

---
+++ src/test/java/com/xuzi/community/MailTests.java
@@ -0,0 +1,24 @@
+package com.xuzi.community;
+
+import com.xuzi.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Test
+    public void sendTextTest(){
+        mailClient.sendMail("nowcoder@sina.com","Text","hhhhhh");
+    }
+
+}
