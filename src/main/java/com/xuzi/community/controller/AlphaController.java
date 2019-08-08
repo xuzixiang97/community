ca499ae118f3569de0d088fd6f956d4a41a3e004
xuzixiang <1090765806@qq.com>
2019-08-08 15:33:16 +0800

    生成验证码，使用kaptcha

--- src/main/java/com/xuzi/community/controller/AlphaController.java
+++ src/main/java/com/xuzi/community/controller/AlphaController.java
@@ -60,4 +60,5 @@ public class AlphaController {
         list.add(map);
         return  list;
     }
+
 }
--- src/main/java/com/xuzi/community/controller/LoginController.java
+++ src/main/java/com/xuzi/community/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.xuzi.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.xuzi.community.entity.User;
 import com.xuzi.community.service.UserService;
 import com.xuzi.community.util.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +13,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @Autowired
     private UserService userService;
@@ -58,6 +72,23 @@ public class LoginController implements CommunityConstant {
             model.addAttribute("target", "/index");
         }
         return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+        // 将验证码存入session
+        session.setAttribute("kaptcha", text);
+
+        // 将突图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            ServletOutputStream outputStream = response.getOutputStream();
+            ImageIO.write(image,"png",outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
