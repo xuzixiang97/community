53a23788676faede21058b4018be10f8cff206d6
xuzixiang <1090765806@qq.com>
2019-08-12 20:02:10 +0800

    使用前缀树算法过滤敏感词

---
+++ src/main/java/com/xuzi/community/util/SensitiveFilter.java
@@ -0,0 +1,150 @@
+package com.xuzi.community.util;
+
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class SensitiveFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(SensitiveFilter.class);
+
+    // 替换符
+    private static final String REPLACEMENT = "***";
+
+    // 根节点
+    private TrieNode rootNode = new TrieNode();
+
+    @PostConstruct
+    public void init() {
+        try (
+                InputStream is = this.getClass().getClassLoader().getResourceAsStream("sensitive-words.txt");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        ) {
+            String keyword;
+            while ((keyword = reader.readLine()) != null) {
+                // 添加到前缀树
+                this.addKeyword(keyword);
+            }
+        } catch (IOException e) {
+            logger.error("加载敏感词文件失败: " + e.getMessage());
+        }
+    }
+
+    // 将一个敏感词添加到前缀树中
+    private void addKeyword(String keyword) {
+        TrieNode tempNode = rootNode;
+
+        for (int i = 0; i < keyword.length(); i++) {
+            char c = keyword.charAt(i);
+            TrieNode node = rootNode.getSubNode(c);
+            if (node == null) {
+                node = new TrieNode();
+                tempNode.addSubNode(c, node);
+            }
+            tempNode = node;
+            // 设置结束标识
+            if (i == keyword.length() - 1) {
+                tempNode.setKeywordEnd(true);
+            }
+        }
+
+    }
+
+    /**
+     * 过滤敏感词
+     * @param text 待过滤的文本
+     * @return 过滤后的文本
+     */
+    public String filter(String text) {
+        if (StringUtils.isBlank(text)) {
+            return null;
+        }
+
+        // 指针1
+        TrieNode tempNode = rootNode;
+        // 指针2
+        int begin = 0;
+        // 指针3
+        int position = 0;
+        // 结果
+        StringBuilder sb = new StringBuilder();
+
+        while (begin < text.length()) {
+            char c = text.charAt(position);
+
+            // 跳过符号
+            if (isSymbol(c)) {
+                if (tempNode == rootNode) {
+                    sb.append(c);
+                    begin++;
+                }
+                position++;
+                continue;
+            }
+
+            // 检查下级节点
+            tempNode = tempNode.getSubNode(c);
+            if (tempNode == null) {
+                sb.append(text.charAt(begin));
+                position = ++begin;
+                tempNode = rootNode;
+            } else if (tempNode.isKeywordEnd()) {
+                sb.append(REPLACEMENT);
+                begin = ++position;
+                tempNode = rootNode;
+            } else {
+                position++;
+            }
+        }
+
+        sb.append(text.substring(begin));
+        return sb.toString();
+    }
+
+    // 判断是否为符号
+    private boolean isSymbol(Character c) {
+        // 0x2E80~0x9FFF 是东亚文字范围
+        return !CharUtils.isAsciiAlphanumeric(c) && (c < 0x2E80 || c > 0x9FFF);
+    }
+
+    // 前缀树
+    private class TrieNode {
+
+        // 结束标识
+        private boolean isKeywordEnd = false;
+
+        // 子节点(key是下级字符,value是下级节点)
+        private Map<Character, TrieNode> subNodes = new HashMap<>();
+
+        public boolean isKeywordEnd() {
+            return isKeywordEnd;
+        }
+
+        public void setKeywordEnd(boolean keywordEnd) {
+            isKeywordEnd = keywordEnd;
+        }
+
+        // 添加子节点
+        public void addSubNode(Character c, TrieNode node) {
+            subNodes.put(c, node);
+        }
+
+        // 获取子节点
+        public TrieNode getSubNode(Character c) {
+            return subNodes.get(c);
+        }
+
+    }
+
+}
