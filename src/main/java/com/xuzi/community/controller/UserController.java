ef07ad26276470b5288b59c471c2400a94b4c8f1
xuzixiang <1090765806@qq.com>
2019-08-21 15:57:48 +0800

    实现了关注和取消关注 修复了主页点击个人主页跳转不到个人主页的bug

--- src/main/java/com/xuzi/community/controller/UserController.java
+++ src/main/java/com/xuzi/community/controller/UserController.java
@@ -2,8 +2,10 @@ package com.xuzi.community.controller;
 
 import com.xuzi.community.annotation.LoginRequired;
 import com.xuzi.community.entity.User;
+import com.xuzi.community.service.FollowService;
 import com.xuzi.community.service.LikeService;
 import com.xuzi.community.service.UserService;
+import com.xuzi.community.util.CommunityConstant;
 import com.xuzi.community.util.CommunityUtil;
 import com.xuzi.community.util.HostHolder;
 import org.apache.commons.lang3.StringUtils;
@@ -26,7 +28,7 @@ import java.io.OutputStream;
 
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements CommunityConstant{
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -47,6 +49,9 @@ public class UserController {
 
     @Autowired
     private LikeService likeService;
+
+    @Autowired
+    private FollowService followService;
 
     /**
      * 跳转用户设置界面
@@ -180,6 +185,19 @@ public class UserController {
         // 点赞数量
         int likeCount = likeService.findUserLikeCount(userId);
         model.addAttribute("likeCount", likeCount);
+        // 关注数量
+        long followeeCount = followService.findFolloweeCount(userId, ENTITY_TYPE_USER);
+        model.addAttribute("followeeCount", followeeCount);
+        // 粉丝数量
+        long followerCount = followService.findFollowerCount(ENTITY_TYPE_USER, userId);
+        model.addAttribute("followerCount", followerCount);
+        // 是否已关注
+        boolean hasFollowed = false;
+        if (hostHolder.getUser() != null) {
+            hasFollowed = followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
+        }
+        model.addAttribute("hasFollowed", hasFollowed);
+
 
         return "/site/profile";
     }
--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -4,11 +4,37 @@ $(function(){
 
 function follow() {
 	var btn = this;
+    var entityId = $("#entityId").val();
+
 	if($(btn).hasClass("btn-info")) {
 		// 关注TA
-		$(btn).text("已关注").removeClass("btn-info").addClass("btn-secondary");
+		$.post(
+			CONTEXT_PATH + "/follow",
+			{"entityType" : "3", "entityId" : entityId},
+            function(data) {
+                data = $.parseJSON(data);
+                if(data.code == 0) {
+                    window.location.reload();
+                } else {
+                    alert(data.msg);
+                }
+            }
+		)
+		//$(btn).text("已关注").removeClass("btn-info").addClass("btn-secondary");
 	} else {
 		// 取消关注
-		$(btn).text("关注TA").removeClass("btn-secondary").addClass("btn-info");
+        $.post(
+            CONTEXT_PATH + "/unfollow",
+            {"entityType":3,"entityId":entityId},
+            function(data) {
+                data = $.parseJSON(data);
+                if(data.code == 0) {
+                    window.location.reload();
+                } else {
+                    alert(data.msg);
+                }
+            }
+        );
+		//$(btn).text("关注TA").removeClass("btn-secondary").addClass("btn-info");
 	}
 }
